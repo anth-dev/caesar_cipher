20d051922c43013bd537672167aac44ba11739c6
Anthony Giles <omgliekpie@gmail.com>
2021-03-08 16:03:15 -0700

    Uploading to github to share code.

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,2 +1,7 @@
-#spec/caesar_cipher_spec.rb
+# spec/caesar_cipher_spec.rb
 
+describe "#caesar_cipher" do
+  it "shifts each character according to the shift factor" do
+    expect(caesar_cipher("a", 1)).to eql("b")
+  end
+end
