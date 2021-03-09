ec2c81cacfac9256dd0d6725992b334b41f4d0ee
Anthony Giles <omgliekpie@gmail.com>
2021-03-08 20:03:31 -0700

    Tested, found bug, fixed bug. (negative shifts less than -26 was erroring)

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
+
+require 'pry'
+
 def caesar_cipher(string, shift)
+
+  shift *= 2
   # make an array containing the complete alphabet
   alphabet = %w[a A b B c C d D e E f F g G h H i I j J k K l L m M n N o O p P q Q r R s S t T u U v V w W x X y Y z Z]
 
@@ -11,6 +16,8 @@ def caesar_cipher(string, shift)
 
   # iterate over the array
   character_array.each do |char|
+    
+ 
     # check to see the character is in the alphabet if it's not then shovel it into
     # the encrypted_string because it's a special character
     if alphabet.index(char).nil?
@@ -21,11 +28,11 @@ def caesar_cipher(string, shift)
       index = alphabet.index(char) + shift
 
       # check to see if we need to loop around the alphabet array
-      if index > alphabet.length - 1
+      if index > alphabet.length - 1 || index.negative?
         remainder = index % 52
         encrypted_string += alphabet[remainder]
-      else
 
+      else
         # shovel the encrypted character into the string
         encrypted_string += alphabet[index]
       end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,7 +1,26 @@
 # spec/caesar_cipher_spec.rb
+require './lib/caesar_cipher'
 
 describe "#caesar_cipher" do
   it "shifts each character according to the shift factor" do
     expect(caesar_cipher("a", 1)).to eql("b")
+  end
+  it "works with mixed case strings" do
+    expect(caesar_cipher("HeLlO", 2)).to eql("JgNnQ")
+  end
+  it "preserves punctuation in the encoded message" do
+    expect(caesar_cipher("Hi!", 3)).to eql("Kl!")
+  end
+  it "can accept negative shifts to decode messages" do
+    expect(caesar_cipher("Jgnnq.", -2)).to eql("Hello.")
+  end
+  it "can loop around the alphabet" do
+    expect(caesar_cipher("xyz", 4)).to eql("bcd")
+  end
+  it "works with large positive shift factors" do
+    expect(caesar_cipher("Hello, World!", 75)).to eql("Ebiil, Tloia!")
+  end
+  it "works with large negative shift factors" do
+    expect(caesar_cipher("Ebiil, Tloia!", -75)).to eql("Hello, World!")
   end
 end
